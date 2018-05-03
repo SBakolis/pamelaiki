c4fd19b7b33fe818f2e33e86fcaeaaf43b82fe1e
zapatistas <30812664+zapatistas@users.noreply.github.com>
2018-05-03 21:39:12 +0300

    uy

--- pamelaiki/app/src/main/java/app/pamelaiki/com/MainActivity.java
+++ pamelaiki/app/src/main/java/app/pamelaiki/com/MainActivity.java
@@ -2,13 +2,6 @@ package app.pamelaiki.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.content.Intent;
-import android.widget.EditText;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 
 import android.widget.ListView;
 
@@ -16,6 +9,25 @@ import java.util.ArrayList;
 
     public class MainActivity extends AppCompatActivity {
 
-       
+        private ListView listView;
+        private sMarketAdapter sMAdapter;
 
-}
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            listView = (ListView)findViewById(R.id.listView0);
+            ArrayList<sMarket> sMarketList = new ArrayList<>();
+            sMarketList.add(new sMarket("Περιστερι", "23km", 0 ,0));
+            sMarketList.add(new sMarket("Αθηνα", "25km", 0 ,0));
+            sMarketList.add(new sMarket("Χαιδαρι", "33km", 0 ,0));
+            sMarketList.add(new sMarket("Νικαια", "40km", 0 ,0));
+
+            sMAdapter = new sMarketAdapter(this,sMarketList);
+            listView.setAdapter(sMAdapter);
+        }
+
+
+
+    }
