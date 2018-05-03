0141254764989eab36acd608d5c28eb4f3fab0de
zapatistas <30812664+zapatistas@users.noreply.github.com>
2018-05-03 21:34:41 +0300

    ff

--- pamelaiki/app/src/main/java/app/pamelaiki/com/MainActivity.java
+++ pamelaiki/app/src/main/java/app/pamelaiki/com/MainActivity.java
@@ -6,8 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.EditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
 import android.widget.ListView;
 
