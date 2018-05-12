ae8ca174641e292c579090492a5cf9c1ad7ec069
SBakolis <s.bakolis@outlook.com>
2018-05-13 02:42:43 +0300

    location functionality
    -request last known location
    -request permissions for location

--- pamelaiki/app/src/main/java/app/pamelaiki/com/MainActivity.java
+++ pamelaiki/app/src/main/java/app/pamelaiki/com/MainActivity.java
@@ -1,5 +1,9 @@
 package app.pamelaiki.com;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -9,9 +13,16 @@ import java.util.ArrayList;
 
 import android.widget.ListView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
+
+import static com.google.android.gms.location.LocationServices.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
         private Calendar sCalendar;
         public String dayLongName;
         private TextView greetText;
+        private TextView locationtest;
+        private FusedLocationProviderClient mFusedLocationClient;
+        public double deviceLatt;
+        public double deviceLong;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +57,31 @@ public class MainActivity extends AppCompatActivity {
 
             greetText = (TextView) findViewById(R.id.greetText);
             greetText.setText("Καλημέρα, σήμερα " + dayLongName + " οι κοντινότερες αγορές είναι:");
+
+            locationtest = findViewById(R.id.locationtest);
+
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+            if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+
+                ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  },
+                        1);
+            }
+            
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                deviceLong = location.getLatitude();
+                                deviceLatt = location.getLongitude();
+                                locationtest.setText("Recent Location " + deviceLong +","+ deviceLatt );
+                            }
+                        }
+
+                    });
+
+
         }
     }
 
