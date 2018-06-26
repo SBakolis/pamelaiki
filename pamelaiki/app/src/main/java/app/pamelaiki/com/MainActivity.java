77b259002d3b288f8c9ecd2948052b6331e2d486
zapatistas <30812664+zapatistas@users.noreply.github.com>
2018-06-26 22:56:39 +0300

    5r

--- pamelaiki/app/src/main/java/app/pamelaiki/com/MainActivity.java
+++ pamelaiki/app/src/main/java/app/pamelaiki/com/MainActivity.java
@@ -1,26 +1,33 @@
 package app.pamelaiki.com;
 
 
-=======
+
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
->>>>>>> master
 
-=======
+
+
 import com.google.android.gms.tasks.OnSuccessListener;
 
->>>>>>> master
+
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
+import static com.google.android.gms.location.LocationServices.*;
 
-=======
+public class MainActivity extends AppCompatActivity {
+
+        private ListView listView;
+        private sMarketAdapter sMAdapter;
+        private Calendar sCalendar;
+        public String dayLongName;
+        private TextView greetText;
         private TextView locationtest;
         private FusedLocationProviderClient mFusedLocationClient;
         public double deviceLatt;
@@ -89,6 +96,5 @@ import java.util.Locale;
         }
     }
 
->>>>>>> master
 
 
